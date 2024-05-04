cd703fefb9301d351c09cd561a53c0952f47d97a
Codebulous <codebulous@gmail.com>
2024-05-05 03:05:58 +0800

    added track id under each player

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -98,6 +98,34 @@ class Tracker:
             lineType=cv2.LINE_4
         )
 
+        rectangle_width = 40
+        rectangle_height = 20
+        x1_rect = x_center - rectangle_width // 2
+        x2_rect = x_center + rectangle_width // 2
+        y1_rect = (y2 - rectangle_height // 2) + 15
+        y2_rect = (y2 + rectangle_height // 2) + 15
+
+        if track_id is not None:
+            cv2.rectangle(frame,
+                          (int(x1_rect), int(y1_rect)),
+                          (int(x2_rect), int(y2_rect)),
+                          color,
+                          cv2.FILLED)
+
+            x1_text = x1_rect + 12
+            if track_id > 99:
+                x1_text -= 10
+
+            cv2.putText(
+                frame,
+                f"{track_id}",
+                (int(x1_text), int(y1_rect + 15)),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.6,
+                (0, 0, 0),
+                2
+            )
+
         return frame
 
     def draw_annotations(self, video_frames, tracks):
